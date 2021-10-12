0e62fdc190a82713026a141b58cc321da172c0e2
vivek992 <55307605+vivek992@users.noreply.github.com>
2021-10-12 17:27:13 +0530

    Added video yt

--- app/src/main/java/com/example/billbook/About.java
+++ app/src/main/java/com/example/billbook/About.java
@@ -22,9 +22,14 @@ public class About extends AppCompatActivity {
                 "6:  If scanner dont work you can add product manually.\n" +
                 "7:  By long press you can delete item\n" +
                 "8:  After scanning all products click on done button.\n" +
-                "9:  Enter customer name & email and click on send\n     button.\n" +
+                "9:  Enter customer name & email and click on send button.\n" +
                 "10:  For details view video given below";
         about_text = findViewById(R.id.about_text);
+//        videoview = findViewById(R.id.videoview);
+//        videoview.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.billbook_video);
+//        MediaController mediaController = new MediaController(this);
+//        mediaController.setAnchorView(videoview);
+//        videoview.setMediaController(mediaController);
         about_text.setText(s);
 
     }
